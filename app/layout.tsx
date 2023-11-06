9bd20c5817c21dffe0953d49b2aad739ed61a88e
Gabriel <1615812+Gabriel-H@users.noreply.github.com>
2023-11-06 17:40:07 +0000

    Add meticulous snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
     <html
       lang="en"
       className="bg-white dark:bg-gray-950 text-black dark:text-white"
-    >
+    >      
+      <head>
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script
+            data-project-id="PLf1fSnB3ptyoNJmCTW5ddBbe0xpWNmimlhiSo8S"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+            />
+      </head>
       <body className={GeistSans.className}>{children}</body>
     </html>
   );
